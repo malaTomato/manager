279b4cd0ad442fa6c9d3f6316477bf57b7967fad
xiongwu <xiongwuhappy@163.com>
2019-05-06 18:24:25 +0800

    加入bootstrap

--- src/main/java/com/neptune/manager/config/ShiroConfig.java
+++ src/main/java/com/neptune/manager/config/ShiroConfig.java
@@ -34,6 +34,8 @@ public class ShiroConfig {
         Map<String,String> filterChainDefinitionMap = new LinkedHashMap<>();
         //配置不会拦截的链接 顺序判断
         filterChainDefinitionMap.put("/static/**","anon");
+        filterChainDefinitionMap.put("/css/**", "anon");
+        filterChainDefinitionMap.put("/js/**", "anon");
         //配置退出过滤器
         filterChainDefinitionMap.put("/logout","logout");
         //<!-- 过滤链定义，从上向下顺序执行，一般将/**放在最为下边 -->:这是一个坑呢，一不小心代码就不好使了;
