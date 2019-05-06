215a5a84cb8c70831fd6053dbaa9a524156a59fd
xiongwu <xiongwuhappy@163.com>
2019-05-06 18:24:03 +0800

    加入bootstrap

--- src/main/java/com/neptune/manager/domain/bean/sys/SysRole.java
+++ src/main/java/com/neptune/manager/domain/bean/sys/SysRole.java
@@ -1,7 +1,9 @@
 package com.neptune.manager.domain.bean.sys;
 
-import java.util.Date;
+import lombok.Data;
 
+import java.util.Date;
+@Data
 public class SysRole {
     private Integer id;
 
@@ -14,52 +16,4 @@ public class SysRole {
     private Date createTime;
 
     private Date updateTime;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role == null ? null : role.trim();
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description == null ? null : description.trim();
-    }
-
-    public Byte getStatus() {
-        return status;
-    }
-
-    public void setStatus(Byte status) {
-        this.status = status;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
 }
--- src/main/java/com/neptune/manager/domain/bean/sys/SysRolePermission.java
+++ src/main/java/com/neptune/manager/domain/bean/sys/SysRolePermission.java
@@ -1,33 +1,12 @@
 package com.neptune.manager.domain.bean.sys;
 
+import lombok.Data;
+
+@Data
 public class SysRolePermission {
     private Integer id;
 
     private Integer permissionId;
 
     private Integer roleId;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getPermissionId() {
-        return permissionId;
-    }
-
-    public void setPermissionId(Integer permissionId) {
-        this.permissionId = permissionId;
-    }
-
-    public Integer getRoleId() {
-        return roleId;
-    }
-
-    public void setRoleId(Integer roleId) {
-        this.roleId = roleId;
-    }
 }
