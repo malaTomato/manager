1715fff53603986d1968e79f0ebbf1916ab91680
xiongwu <xiongwuhappy@163.com>
2019-05-06 18:24:16 +0800

    加入bootstrap

--- src/main/java/com/neptune/manager/ManagerApplication.java
+++ src/main/java/com/neptune/manager/ManagerApplication.java
@@ -2,9 +2,9 @@ package com.neptune.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-
 public class ManagerApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/neptune/manager/web/SysUserController.java
+++ src/main/java/com/neptune/manager/web/SysUserController.java
@@ -1,16 +1,23 @@
 package com.neptune.manager.web;
 
 import com.alibaba.fastjson.JSONObject;
+import com.neptune.manager.domain.bean.sys.SysUser;
+import com.neptune.manager.service.sys.SysUserService;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -20,6 +27,9 @@ import java.util.Map;
 public class SysUserController {
 
     private static final Logger LOG = LoggerFactory.getLogger(SysUserController.class);
+
+    @Autowired
+    private SysUserService sysUserService;
 
     @RequestMapping({"/","/index"})
     public String index(){
@@ -60,16 +70,11 @@ public class SysUserController {
 
     @RequestMapping("/userInfoAdd")
     @RequiresPermissions("userInfo:add")
-    public String userInfoAdd(){
+    public String userInfoAdd(@Valid @RequestBody SysUser sysUser){
         LOG.info("-->>userInfoAdd<<--");
         LOG.info(JSONObject.toJSONString(SecurityUtils.getSubject().getPrincipals()));
-//
-//        System.out.println("HomeController.login()");
-//        // 登录失败从request中获取shiro处理的异常信息。
-//        // shiroLoginFailure:就是shiro异常类的全类名.
-//        String exception = (String) request.getAttribute("shiroLoginFailure");
-//        System.out.println("exception=" + exception);
-//        String msg = "";
+
+        sysUserService.addSysUser(sysUser).get();
 
         return "/userInfoAdd";
     }
@@ -104,5 +109,17 @@ public class SysUserController {
 //        String msg = "";
 
         return "/userInfoView";
+    }
+
+    public static void main(String[] args) {
+        Map<String, SysUser> map = new HashMap<>();
+        SysUser sysUser = new SysUser();
+        sysUser.setPassword("123");
+        map.put("11",sysUser);
+        System.out.println(JSONObject.toJSONString(map));
+        sysUser.setPassword("34556");
+        System.out.println(JSONObject.toJSONString(map));
+        BigDecimal bigDecimal = new BigDecimal("0.09");
+        System.out.println(bigDecimal);
     }
 }
